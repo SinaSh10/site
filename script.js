44945fdb57d63508b649179c22dc1093ae139071
SinaSh10 <sinash10.job@gmail.com>
2023-08-28 14:25:55 +0330

    second part of main created

--- script.js
+++ script.js
@@ -16,3 +16,19 @@ setInterval(() => {
     setSlide(`slide${slideindex}`,slideindex)
     
 }, 4000);
+let remainingTime=60000
+function setTime(){
+    if(remainingTime==0)return
+    let h=Math.floor(remainingTime/3600)
+    let m=Math.floor((remainingTime%3600)/60)
+    let s=(remainingTime%3600)%60
+    document.querySelector('#hour').innerHTML=h
+    document.querySelector('#miniute').innerHTML=m
+    document.querySelector('#second').innerHTML=s
+}
+setInterval(
+    ()=>{
+        remainingTime-=1
+        setTime()
+    }
+, 1000);
